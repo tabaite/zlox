0fa6f7360beead2d12d9e8f20881f78cc775466a
tabaite <105614378+tabaite@users.noreply.github.com>
2025-07-29 13:49:01 -0500

    Implement printing expressions

--- src/parsing.zig
+++ src/parsing.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const Expression = union(enum) {
     literal: union(enum) {
         number: f32,
@@ -28,3 +30,45 @@ pub const Expression = union(enum) {
     },
     grouping: struct { expr: *Expression },
 };
+
+pub fn printExpression(expr: *Expression, out: std.io.AnyWriter) !void {
+    switch (expr.*) {
+        .literal => |l| switch (l) {
+            .number => |num| try out.print("{d}", .{num}),
+            .string => |str| try out.print("{s}", .{str}),
+            .true => try out.write("true"),
+            .false => try out.write("false"),
+            .nil => try out.write("nil"),
+        },
+        .unary => |u| {
+            switch (u.operation) {
+                .Negate => try out.write("(- "),
+                .NegateBool => try out.write("(! "),
+            }
+            try printExpression(u.expr, out);
+            try out.write(")");
+        },
+        .binary => |b| {
+            switch (b.operation) {
+                .Equality => try out.write("(== "),
+                .NotEquality => try out.write("(!= "),
+                .Greater => try out.write("(> "),
+                .GreaterEqual => try out.write("(>= "),
+                .Less => try out.write("(< "),
+                .LessEqual => try out.write("(<= "),
+                .Add => try out.write("(+ "),
+                .Subtract => try out.write("(- "),
+                .Multiply => try out.write("(* "),
+                .Divide => try out.write("(/ "),
+            }
+            try printExpression(b.left, out);
+            try printExpression(b.right, out);
+            try out.write(")");
+        },
+        .grouping => |g| {
+            try out.write("(group ");
+            try printExpression(g.expr, out);
+            try out.write(")");
+        },
+    }
+}
--- src/root.zig
+++ src/root.zig
@@ -1,1 +1,2 @@
 pub const scanning = @import("scanning.zig");
+pub const parsing = @import("parsing.zig");
