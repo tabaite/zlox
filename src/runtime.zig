e06118572479179c3e3a93e2ce91e7e9374a9d01
tabaite <105614378+tabaite@users.noreply.github.com>
2025-09-16 10:40:15 -0500

    declarations should be operational

--- src/runtime.zig
+++ src/runtime.zig
@@ -7,6 +7,9 @@ const Allocator = std.mem.Allocator;
 
 pub const RuntimeError = error{
     StackOverflow,
+    OutOfStackBounds,
+    UndeclaredVariableAccessed,
+    VariableAlreadyDeclared,
 };
 
 pub const RuntimeIndex = u24;
@@ -36,6 +39,7 @@ pub const VarInternal = struct {
 };
 
 pub const VarStack = struct {
+    const NILHANDLE: VarHandle = .{ .type = .nil, .handle = 0 };
     const STACKSIZE: usize = 16777215;
 
     items: []VarInternal,
@@ -43,9 +47,11 @@ pub const VarStack = struct {
     used: u24,
 
     pub fn init(allocator: Allocator, stringAllocator: Allocator) !VarStack {
+        const items = try allocator.alloc(VarInternal, STACKSIZE);
+        items[0] = .{ .type = .nil, .extra = 0 };
         return .{
-            .items = try allocator.alloc(VarInternal, STACKSIZE),
-            .used = 0,
+            .items = items,
+            .used = 1,
             .stringAllocator = stringAllocator,
         };
     }
@@ -70,33 +76,94 @@ pub const VarStack = struct {
         return self.push(.bool, @intCast(@intFromBool(b)));
     }
 
+    pub fn set(self: *VarStack, handle: VarHandle, new: parsing.Literal) !void {
+        if (handle.handle > self.used) {
+            return RuntimeError.OutOfStackBounds;
+        }
+
+        const item = &self.items[handle.handle];
+        if (item.type == .string) {
+            self.stringAllocator.free(@ptrFromInt(@as(usize, item.extra)));
+        }
+
+        switch (new) {
+            .number => |n| {
+                item.* = VarInternal{ .type = .number, .extra = @bitCast(n) };
+            },
+            .bool => |b| {
+                item.* = VarInternal{ .type = .bool, .extra = @intCast(@intFromBool(b)) };
+            },
+            .nil => {
+                item.* = VarInternal{ .type = .nil, .extra = 0 };
+            },
+            .string => |s| {
+                const strPtr = try self.stringAllocator.create([]u8);
+                strPtr.* = s;
+                item.* = VarInternal{ .type = .string, .extra = @bitCast(strPtr) };
+            },
+        }
+    }
+
     // We might not need to dupe the string. I'm still undecided on when the
     // lifetime of the input file ends.
-
     pub fn pushString(self: *VarStack, str: []u8) !VarHandle {
         const newString = try self.stringAllocator.dupe(u8, str);
-        return self.push(.string, @intCast(@intFromPtr(&newString)));
+        // this is fine... i think
+        const strPtr = try self.stringAllocator.create([]u8);
+        strPtr.* = newString;
+        return self.push(.string, @bitCast(strPtr));
     }
 
     pub fn pop(self: *VarStack) void {
-        if (self.used > 0) {
+        const handle = self.items[self.used];
+        if (handle.type == .string) {
+            self.stringAllocator.free(@ptrFromInt(@as(usize, handle.extra)));
+        }
+
+        // The first element is our nil handle.
+        if (self.used > 1) {
             self.used -= 1;
         }
     }
 };
 
 pub const Runtime = struct {
+    varRegistryAllocator: Allocator,
+    varRegistry: std.StringHashMap(VarHandle),
     variableStack: VarStack,
     pub fn init(allocator: Allocator, stringAllocator: Allocator) !Runtime {
-        return .{ .variableStack = try VarStack.init(allocator, stringAllocator) };
+        return .{
+            .varRegistryAllocator = allocator,
+            .varRegistry = std.StringHashMap(VarHandle).init(allocator),
+            .variableStack = try VarStack.init(allocator, stringAllocator),
+        };
     }
 
-    pub fn push(self: *Runtime, v: parsing.Literal) !VarHandle {
+    pub fn declare(self: *Runtime, name: []u8, v: ?parsing.Literal) !VarHandle {
+        const handle = if (v != null) try self.push(v orelse unreachable) else VarHandle.NILHANDLE;
+        const result = try self.varRegistry.getOrPut(name);
+        if (result.found_existing) {
+            return RuntimeError.VariableAlreadyDeclared;
+        } else {
+            result.value_ptr.* = handle;
+        }
+    }
+
+    pub fn set(self: *Runtime, name: []u8, new: parsing.Literal) !void {
+        const result = try self.varRegistry.getOrPut(name);
+        if (result.found_existing) {
+            const handle = result.value_ptr.*;
+            self.variableStack.set(handle, new);
+        } else {
+            return RuntimeError.UndeclaredVariableAccessed;
+        }
+    }
+
+    fn push(self: *Runtime, v: parsing.Literal) !VarHandle {
         switch (v) {
             .number => |n| return self.variableStack.pushNumber(n),
             .string => |s| return self.variableStack.pushString(s),
-            .true => return self.variableStack.pushBool(true),
-            .false => return self.variableStack.pushBool(false),
+            .bool => |b| return self.variableStack.pushBool(b),
             // bro...... trust me
             .nil => unreachable,
         }
